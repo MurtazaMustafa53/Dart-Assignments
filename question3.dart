3c681e1d3d538a84ce3774626935a7ce7aa3532f
Murtaza <murtazamandasore@gmail.com>
2025-07-22 15:40:35 +0500

    question 3 done

---
+++ question3.dart
@@ -0,0 +1,16 @@
+void main() {
+  /*
+   create a list  of name, class, roll no, grade, percentage. And print.
+   */
+  List students = [
+    ["Murtaza", "Hassan", "Mustafa"],
+    [12, 12, 12],
+    [1, 2, 3],
+    ["A", "C", "B"],
+    [81, 66, 72],
+  ];
+  print(students[0][1]);
+  print(students[1][1]);
+  print(students[2][1]);
+  print(students[3][1]);
+}
