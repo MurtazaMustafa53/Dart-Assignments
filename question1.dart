642e82c1dce984bd39e59a23bff34c79737b5398
Murtaza <murtazamandasore@gmail.com>
2025-07-22 15:28:10 +0500

    question 1 Done

---
+++ question1.dart
@@ -0,0 +1,7 @@
+void main() {
+  /*
+   Create a list of names and print all names using list.
+   */
+  List names = ["Hassan", "Murtaza", "Ali", "Mustafa", "Mohammad", "Hussain"];
+  print(names);
+}
