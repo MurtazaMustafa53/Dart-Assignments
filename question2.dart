16e9b26b1118fe6e40589bd198dcc7c67c3c7f05
Murtaza <murtazamandasore@gmail.com>
2025-07-22 15:32:16 +0500

    Question 2 Done

---
+++ question2.dart
@@ -0,0 +1,15 @@
+void main() {
+  /*
+  Create a list of Days and print only  Sunday
+   */
+  List Days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  print(Days[0]);
+}
